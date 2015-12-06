25573ef8b939386600f28422b88002e2c6b10bff
Carl <spotteprong@gmail.com>
2015-12-06 00:48:43 +0000

    Added association and validation between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,8 @@
 class Stock < ActiveRecord::Base
-    
+    belongs_to :user
     validates :barcode, :name, :description, presence: true 
     validates :barcode, uniqueness: true
     validates :quantity, :price, presence: true
+    validates :user_id, presence: true
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205234106) do
+ActiveRecord::Schema.define(version: 20151206001852) do
 
   create_table "products", force: :cascade do |t|
     t.string "product_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20151205234106) do
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
