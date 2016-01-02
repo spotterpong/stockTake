6d43a7f13527a3528d14e3d9dcb137360a3b850c
Carl <spotteprong@gmail.com>
2016-01-02 00:40:52 +0000

    Fixed migration issues in production

--- db/migrate/20160101191320_add_comment_body_and_contact_id_to_comments_table.rb
+++
@@ -1,6 +0,0 @@
-class AddCommentBodyAndContactIdToCommentsTable < ActiveRecord::Migration
-  def change
-    add_column :comments, :comment_body, :string
-    add_column :comments, :contact_id, :string
-  end
-end
